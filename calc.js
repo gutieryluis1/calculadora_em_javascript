f8e9a22879ca9e94616c5a458a3c8c3f0c4c1cc9
Gutiery Luis <gutiery.luis1uepb@gmail.com>
2022-08-29 22:35:00 -0300

    estudando modulos em node.js

---
+++ calc.js
@@ -0,0 +1,9 @@
+var somaFunc = require('./somar')
+var subFunc = require('./sub')
+var multiFunc = require('./multi')
+var divFunc = require('./div')
+
+console.log(somaFunc(3,5))
+console.log(subFunc(2,6))
+console.log(multiFunc(10,30))
+console.log(divFunc(15,2))
